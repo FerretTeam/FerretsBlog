43f01d0339cdece4c1885cefcf394f80162c2bac
shuq3 <154421897@qq.com>
2017-01-22 16:45:24 +0800

    sass

--- src/component/article.component.ts
+++ src/component/article.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.sass']
 })
 export class ArticleComponent {
   article = 'right!';
--- src/component/person.component.ts
+++ src/component/person.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'person',
   templateUrl: './person.component.html',
-  styleUrls: ['./person.component.css']
+  styleUrls: ['./person.component.sass']
 })
 export class PersonComponent {
   name: string;
