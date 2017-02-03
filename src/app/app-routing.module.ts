752f589026b1cf5fa1b254255c9f882a9d293c53
shuq3 <154421897@qq.com>
2017-02-03 13:29:42 +0800

    修改

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,6 @@ import { ModuleWithProviders } from '@angular/core';
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// import { HomeComponent } from '../components/home.component';
-// import { ArticleComponent } from '../components/article.component';
-// import { ProfileComponent } from '../components/profile.component';
-// import { AppComponent } from './app.component';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
