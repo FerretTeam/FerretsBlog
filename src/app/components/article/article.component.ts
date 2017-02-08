2bc4a42c5402476125b66e461a8a41f2db117138
刘忍 <18826073587@163.com>
2017-02-08 15:53:22 +0800

    一些小的修改

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,7 +1,6 @@
 import 'rxjs/add/operator/switchMap';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router} from '@angular/router';
-import { Location }               from '@angular/common';
 import { Article } from './article';
 import { ArticleService } from '../../services/article/article.service';
 import { AuthService } from '../../services/auth/auth.service';
@@ -16,16 +15,17 @@ export class ArticleComponent implements OnInit {
   article: Article;
 
   constructor(private articleService: ArticleService, private router: Router,
-    private location: Location,private activatedRoute: ActivatedRoute, private authService: AuthService) {
+              private activatedRoute: ActivatedRoute, private authService: AuthService) {
     // 如果未登录，则跳转至 /welcome 页面
     if (this.authService.getUserInfo() == null)
       this.router.navigate(['/welcome']);
+    // 取回文章的信息
+    this.activatedRoute.params.subscribe(params => {
+      this.article = this.articleService.getArticle(params['id']);
+    });
   }
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe(params => {
-        this.article = this.articleService.getArticle(+params['id']);
-      });
   }
 
 }
