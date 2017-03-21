e315788c865b2edf432aca2e60dbf8cea270f3ae
刘忍 <18826073587@163.com>
2017-03-21 11:30:16 +0800

    解决脚本中对 css 文件进行压缩导致的错误

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,8 @@ echo 正在优化项目中的图片...
 cd dist/assets/images
 open -a ImageOptim .
 
-# 压缩代码文件的大小
-echo 正在压缩代码文件...
+# 压缩 js 文件的大小
+echo 正在压缩 js 文件...
 cd ../..
 for file in ./*
 do
@@ -19,18 +19,6 @@ do
     # 压缩 js 文件
     echo 压缩 $file
     uglifyjs $file --screw-ie8 --compress --mangle --output $file
-  fi
-  if [[ $file == *.css ]]
-  then
-    # 压缩 css 文件
-    echo 压缩 $file
-    uglifycss $file > $file
-  fi
-  if [[ $file == *.html ]]
-  then
-    # 压缩 html 文件
-    echo 压缩 $file
-    html-minifier --input-dir $file --output-dir $file
   fi
 done
 
